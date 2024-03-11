6e9d23ccf99101e1daaa91273a8f6176f2fa77f2
LeMinhThong <137277487+LeMinhThong@users.noreply.github.com>
2024-03-11 23:10:01 +0700

    Update ALU_8bit.v
    change Cout from 8bit wide to 1bit

--- CMOS_ALU_8bit/CMOS_ALU_8bit.srcs/sources_1/new/ALU_8bit.v
+++ CMOS_ALU_8bit/CMOS_ALU_8bit.srcs/sources_1/new/ALU_8bit.v
@@ -22,16 +22,19 @@
 
 module ALU_8bit(
     input Ci, [7:0] A, [7:0] B, [0:1] F,
-    output [7:0] Out, [7:0]Co
+    output [7:0] Out,
+    output Cout
     );
+
+    wire [6:0] co;
     
-    ALU_1bit U0 (A[0], B[0], Ci, F[0:1], Out[0], Co[0]);
-    ALU_1bit U1 (A[1], B[1], Co[0], F[0:1], Out[1], Co[1]);
-    ALU_1bit U2 (A[2], B[2], Co[1], F[0:1], Out[2], Co[2]);
-    ALU_1bit U3 (A[3], B[3], Co[2], F[0:1], Out[3], Co[3]);
-    ALU_1bit U4 (A[4], B[4], Co[3], F[0:1], Out[4], Co[4]);
-    ALU_1bit U5 (A[5], B[5], Co[4], F[0:1], Out[5], Co[5]);
-    ALU_1bit U6 (A[6], B[6], Co[5], F[0:1], Out[6], Co[6]);
-    ALU_1bit U7 (A[7], B[7], Co[6], F[0:1], Out[7], Co[7]);
+    ALU_1bit U0 (A[0], B[0], Ci,    F[0:1], Out[0], co[0]);
+    ALU_1bit U1 (A[1], B[1], co[0], F[0:1], Out[1], co[1]);
+    ALU_1bit U2 (A[2], B[2], co[1], F[0:1], Out[2], co[2]);
+    ALU_1bit U3 (A[3], B[3], co[2], F[0:1], Out[3], co[3]);
+    ALU_1bit U4 (A[4], B[4], co[3], F[0:1], Out[4], co[4]);
+    ALU_1bit U5 (A[5], B[5], co[4], F[0:1], Out[5], co[5]);
+    ALU_1bit U6 (A[6], B[6], co[5], F[0:1], Out[6], co[6]);
+    ALU_1bit U7 (A[7], B[7], co[6], F[0:1], Out[7], Cout);
     
 endmodule
